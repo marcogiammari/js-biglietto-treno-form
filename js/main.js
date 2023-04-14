48e443e43c50d7aafbaef4804f79200f9bd83baa
marcogiammari <marcogiammari@gmail.com>
2023-04-14 16:40:59 +0200

    add reset button + math.random elements

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ function calcPrice() {
     let km = parseInt(document.getElementById("km").value);
     let age = document.getElementById("age").value;
     let name = document.getElementById("name").value;
-    let price = km*0.21;
-    if (isNaN(km)) {
+    let price = km * 0.21;
+    if (isNaN(km) || name == "" || age == "") {
         retry = confirm("Oops, alcuni dati sono errati.");
     }
     else {
@@ -18,7 +18,19 @@ function calcPrice() {
         }
         price = parseFloat(price).toFixed(2);
         document.getElementById("price").innerText = price + " €";
-        document.getElementById("passengerName").innerText = name;     
+        document.getElementById("passengerName").innerText = name;
+        document.getElementById("fare").innerText = age;
+        randomCar = Math.floor(Math.random() * 9 + 1);
+        document.getElementById("car").innerHTML = randomCar;
+        randomCP = Math.floor(Math.random() * 10000 + 10000);
+        document.getElementById("cp").innerHTML = randomCP;
     }
 }
 
+
+btnReset = document.getElementById("resetBtn");
+btnReset.addEventListener("click", reloadPage);
+
+function reloadPage() {
+    location.reload();
+}
